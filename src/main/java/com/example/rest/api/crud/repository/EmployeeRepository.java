9e55003bc1c373222c5fd4b2d5a1f762f884000f
Pasindu Dilshan Dimbulana <pasindudi@mobitel.lk>
2024-11-28 15:06:06 +0530

    udpate code

--- src/main/java/com/example/rest/api/crud/repository/EmployeeRepository.java
+++ src/main/java/com/example/rest/api/crud/repository/EmployeeRepository.java
@@ -1,10 +1,10 @@
 package com.example.rest.api.crud.repository;
 
 import com.example.rest.api.crud.entity.Employee;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface EmployeeRepository {
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 
     List<Employee> findAll();
 
